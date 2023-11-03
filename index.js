87d71330960c201c546fa8d0f6a2f5c65a627ede
Script Devil <71324001+Benceplays@users.noreply.github.com>
2023-11-03 08:32:35 +0100

    par cucc

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("asd");
+const express = require('express');
+const mysql = require('mysql');
+const app    = express();
+const port   = 3000;
+app.use(express.static('public')); 
+
+const conn = mysql.createConnection( {
+  host: 'localhost',
+  user: 'wildemhu',
+  port: "3306",
+  password: 'Vw69b?(W4mmL',
+  database: 'wildemhu_wildem'
+});
+
+app.listen(port, function () { console.log(`bs app listening at http://localhost:${port}`); });
